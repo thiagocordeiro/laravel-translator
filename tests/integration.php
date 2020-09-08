67131f184df8b5353221d4852e2c7232ccdb02f3
Thiago Cordeiro <thiagoguetten@gmail.com>
2020-09-08 15:06:01 -0300

    Integration test (#32)

--- tests/integration.php
+++ tests/integration.php
@@ -1,28 +1,32 @@
 <?php declare(strict_types=1);
 
 /**
- |--------------------------------------------------------------------------
- | Integration test
- |--------------------------------------------------------------------------
- | This test will be used by travis to check if installation and scan work properly (see .travis.yml)
+ * |--------------------------------------------------------------------------
+ * | Integration test
+ * |--------------------------------------------------------------------------
+ * | This test will be used by travis to check if installation and scan work properly (see .travis.yml)
  */
-$expected = trim(json_encode([
-    'Underscore: :foo, :bar' => '',
-    'Lang: :foo, :bar' => '',
-    'Welcome, :name' => '',
-    'Trip to :planet, check-in opens :time' => '',
-    'Check offers to :planet' => '',
-    'Translations should also work with double quotes.' => '',
-    'Shouldn\'t escaped quotes within strings also be correctly added?' => '',
-    'Same goes for "double quotes".' => '',
-    'String using (parentheses).' => '',
-    "Double quoted string using \"double quotes\", and C-style escape sequences.\n\t\\" => '',
-], JSON_PRETTY_PRINT));
 
-$received = trim(file_get_contents("resources/lang/pt-br.json"));
+$diff = array_diff(
+    [
+        'Underscore: :foo, :bar' => '',
+        'Lang: :foo, :bar' => '',
+        'Welcome, :name' => '',
+        'Trip to :planet, check-in opens :time' => '',
+        'Check offers to :planet' => '',
+        'Translations should also work with double quotes.' => '',
+        'Shouldn\'t escaped quotes within strings also be correctly added?' => '',
+        'Same goes for "double quotes".' => '',
+        'String using (parentheses).' => '',
+        "Double quoted string using \"double quotes\", and C-style escape sequences.\n\t\\" => '',
+    ],
+    json_decode(file_get_contents("resources/lang/pt-br.json"), true)
+);
 
-if ($expected !== $received) {
-    throw new Exception(sprintf("Keys scanned does not match, expected: %s, received: %s", $expected, $received));
+if (!empty($diff)) {
+    throw new Exception(
+        sprintf("Keys scanned does not match by the diff:\n%s\n", print_r($diff, true))
+    );
 }
 
 echo 'Integration works :)';
